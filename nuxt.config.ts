6b5180e779ff5d8c4ff3857f024e0c3015f8c83d
Anatoly Frolov <44891639+anafro@users.noreply.github.com>
2024-07-16 08:06:57 +1000

    chore: change indent from `2` to `4`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: false },
+    compatibilityDate: '2024-04-03',
+    devtools: { enabled: false },
 
-  vite: {
-    css: {
-      preprocessorOptions: {
-        sass: {
-          additionalData: `@import "~/assets/global-styles.sass"`
+    vite: {
+        css: {
+            preprocessorOptions: {
+                sass: {
+                    additionalData: `@import "~/assets/global-styles.sass"`
+                }
+            }
         }
-      }
-    }
-  },
+    },
+
+    eslint: {
+        config: {
+            stylistic: {
+                indent: 'tab',
+                semi: true,
+            }
+        }
+    },
+
+    modules: ["@nuxt/eslint"]
 })
