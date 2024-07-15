e3258e998455713c3434dc6b19416d69e80afba0
Anatoly Frolov <44891639+anafro@users.noreply.github.com>
2024-07-16 07:56:30 +1000

    chore: add sass & global styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        sass: {
+          additionalData: `@import "~/assets/global-styles.sass"`
+        }
+      }
+    }
+  },
 })
