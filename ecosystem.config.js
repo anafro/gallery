d19881e6899dcbfcd5e5894d7608a7b0f0d6329b
Anatoly <contact@anafro.ru>
2024-09-24 17:40:26 +1000

    feat: add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [
+        {
+            name: 'gallery',
+            port: '3000',
+            exec_mode: 'cluster',
+            instances: 'max',
+            script: './.output/server/index.mjs'
+        }
+    ]
+}
